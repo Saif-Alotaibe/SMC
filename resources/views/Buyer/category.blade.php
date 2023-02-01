78d210f5d12324f0bca89e43863a6568580596cd
Saif-Alotaibe <safe1otb@gmail.com>
2023-02-01 18:53:52 +0300

    Fix picture style

--- resources/views/Buyer/category.blade.php
+++ resources/views/Buyer/category.blade.php
@@ -97,7 +97,7 @@
                     <div class="d-flex flex-wrap card-container">
                         @foreach ($categoryPosts as $post)
                             <div class="card col-md-4 col-sm-12" style="width: 22rem; ">
-                                    <img src="{{$post->photos->first()->url}}" style="height: 50%; width: 50%;" alt="...">
+                                    <img src="{{$post->photos->first()->url}}" style="height: 50%; width: 50%; margin:auto;" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $post->title }}</h5>
                                     <h6 class="mt-2">Price {{ $post->price }}</h6>
